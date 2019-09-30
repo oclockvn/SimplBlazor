8f9bd7ed705955d9fc5b6c3fe092e1d6dd7617ab
Quang Phan <oclockvn@gmail.com>
2019-09-30 23:09:02 +0700

    add product model

--- src/SimplBlazor.Shared/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace SimplBlazor.Shared
-{
-    public class Class1
-    {
-    }
-}
---
+++ src/SimplBlazor.Shared/Models/Product.cs
@@ -0,0 +1,11 @@
+namespace SimplBlazor.Shared.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Specification { get; set; }
+        public string Details { get; set; }
+    }
+}
