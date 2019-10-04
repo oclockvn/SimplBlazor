eac42848ff4b45295d3ee1d5782d51d094059aac
Quang Phan <oclockvn@gmail.com>
2019-10-04 08:31:31 +0700

    add product component

--- src/SimplBlazor.Shared/Models/Product.cs
+++ src/SimplBlazor.Shared/Models/Product.cs
@@ -4,8 +4,12 @@ namespace SimplBlazor.Shared.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Thumbnail { get; set; }
+        public string Image { get; set; } // just a demo so don't blame it :)
         public decimal Price { get; set; }
+        public decimal? OldPrice { get; set; }
         public string Specification { get; set; }
         public string Details { get; set; }
+        public int Star { get; set; }
     }
 }
