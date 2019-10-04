7aec22fe9c7aeb580807df30212b5384b1616c91
Quang Phan <oclockvn@gmail.com>
2019-10-04 18:26:12 +0700

    allow cors

--- src/SimplBlazor.Api/Startup.cs
+++ src/SimplBlazor.Api/Startup.cs
@@ -1,15 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace SimplBlazor.Api
 {
@@ -41,6 +34,7 @@ namespace SimplBlazor.Api
             app.UseRouting();
 
             app.UseAuthorization();
+            app.UseCors(p => p.WithOrigins("*"));
 
             app.UseEndpoints(endpoints =>
             {
