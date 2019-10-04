ee52decde6a509030d0c97e207d19a1696de8a94
Quang Phan <oclockvn@gmail.com>
2019-10-04 22:24:43 +0700

    add product slug

--- src/SimplBlazor.Shared/Models/Product.cs
+++ src/SimplBlazor.Shared/Models/Product.cs
@@ -4,6 +4,7 @@ namespace SimplBlazor.Shared.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Slug { get; set; }
         public string Thumbnail { get; set; }
         public string Image { get; set; } // just a demo so don't blame it :)
         public decimal Price { get; set; }
